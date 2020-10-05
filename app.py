051c98adf4a7bddd4654a8b138edaf15f71a3816
jongmok1031 <jongmok1031@gmail.com>
2020-10-05 16:19:32 +0900

    1600

--- app.py
+++ app.py
@@ -17,4 +17,4 @@ api.add_resource(Rest, '/')
     
 if __name__ == '__main__':
     app.run(debug = True, host = '127.0.0.1', port = 8080)       
-####1-19 ㄹㅇ restful객체야
+####1-19 ㄹㅇ restful객체야  casheable
